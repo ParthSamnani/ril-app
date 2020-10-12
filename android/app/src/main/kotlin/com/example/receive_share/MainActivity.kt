c9b4d35ab5ae3c19440a791cd8a8e8273c7297d1
Parth Samnani <samnaniparth@gmail.com>
2020-10-13 02:21:55 +0530

    receiving text and images

---
+++ android/app/src/main/kotlin/com/example/receive_share/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.receive_share
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:receive_sharing_intent/receive_sharing_intent.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  StreamSubscription _intentDataStreamSubscription;
+  List<SharedMediaFile> _sharedFiles;
+  String _sharedText;
+  String filePath;
+  @override
+  void initState() {
+    super.initState();
+
+    // For sharing images coming from outside the app while the app is in the memory
+    _intentDataStreamSubscription =
+        ReceiveSharingIntent.getMediaStream().listen((List<SharedMediaFile> value) {
+          setState(() {
+            print("Shared:" + (_sharedFiles?.map((f)=> f.path)?.join(",") ?? ""));
+            _sharedFiles = value;
+          });
+        }, onError: (err) {
+          print("getIntentDataStream error: $err");
+        });
+
+    // For sharing images coming from outside the app while the app is closed
+    ReceiveSharingIntent.getInitialMedia().then((List<SharedMediaFile> value) {
+      setState(() {
+        _sharedFiles = value;
+        filePath = _sharedFiles?.map((f)=> f.path)?.join(",") ?? "";
+      });
+    });
+    // For sharing or opening urls/text coming from outside the app while the app is in the memory
+    _intentDataStreamSubscription =
+        ReceiveSharingIntent.getTextStream().listen((String value) {
+          setState(() {
+            _sharedText = value;
+          });
+        }, onError: (err) {
+          print("getLinkStream error: $err");
+        });
+
+    // For sharing or opening urls/text coming from outside the app while the app is closed
+    ReceiveSharingIntent.getInitialText().then((String value) {
+      setState(() {
+        _sharedText = value;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _intentDataStreamSubscription.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const textStyleBold = const TextStyle(fontWeight: FontWeight.bold,fontSize: 20);
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Read It Later'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text("Shared Image:", style: textStyleBold),
+              Image.file(File(filePath !=null ? filePath : ""),height: 400,),
+              SizedBox(height: 50),
+              Text("Shared urls/text:", style: textStyleBold),
+              Text(_sharedText ?? ""),
+              SizedBox(height: 100),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
